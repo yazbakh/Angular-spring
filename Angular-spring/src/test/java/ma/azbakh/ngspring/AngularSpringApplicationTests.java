b6ec10feb298f4d1d677df2127184c7c448ebad5
y.azbakh <y.azbakh@Mikos-Lap.gfgmbh.local>
2017-02-08 14:27:20 +0100

    First commit

---
+++ Angular-spring/src/test/java/ma/azbakh/ngspring/AngularSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package ma.azbakh.ngspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AngularSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
